e71254b415f2f92162058a87141cb35e9ab22594
PaulaPousa <paula.pousa@gmail.com>
2021-09-20 11:55:18 +0200

    call is_unique

--- Exercises/src/main.rs
+++ Exercises/src/main.rs
@@ -1,3 +1,10 @@
+extern crate Exercises;
+
 fn main() {
-    println!("Hello, world!");
+
+    // ---------- ARRAYS_STRINGS FUNTIONS CALLS ---------- 
+    Exercises::arrays_strings::is_unique_array(String::from("string"));
+    let result = Exercises::arrays_strings::is_unique_hashmap(String::from("string"));
+    println!("El resultado es: {}", result);
+
 }
